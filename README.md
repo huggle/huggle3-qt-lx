058adc51582a04f74aea8952088e5066cf6f9373
Petr Bena <benapetr@gmail.com>
2013-09-18 10:24:14 +0200

    compatibility with qt5

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ huggle3-qt-lx
 =============
 
 Huggle 3 QT-LX is an anti-vandalism tool for use on Wikipedia and other Wikimedia projects
+
+
+Building
+=========
+
+Windows:
+
+* Download QT Creator
+* Open huggle.pro in that and hit build
+
+Linux:
+
+* sudo apt-get install qt-qmake
+* qmake
+* make
--- huggle/processlist.cpp
+++ huggle/processlist.cpp
@@ -20,7 +20,13 @@ ProcessList::ProcessList(QWidget *parent) : QDockWidget(parent), ui(new Ui::Proc
     ui->tableWidget->setHorizontalHeaderLabels(header);
     ui->tableWidget->verticalHeader()->setVisible(false);
     ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    ui->tableWidget->horizontalHeader()->setResizeMode(QHeaderView::ResizeToContents);
+#if QT_VERSION >= 0x050000
+// Qt5 code
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+#else
+// Qt4 code
+    ui->tableWidget->horizontalHeader()->setsetResizeMode(QHeaderView::ResizeToContents);
+#endif
     //ui->tableWidget->horizontalHeaderItem(0)->setSizeHint(QSize(20,-1));
     ui->tableWidget->setShowGrid(false);
     this->Removed = new QList<ProcessListRemovedItem*> ();
