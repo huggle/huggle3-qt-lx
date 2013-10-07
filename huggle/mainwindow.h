339dec3bb220da45f0b9635ac647fbed6a2fddd1
Aidan <jamie.fell@btinternet.com>
2013-10-07 20:38:27 +0100

    changes n' stuff

--- huggle/mainwindow.h
+++ huggle/mainwindow.h
@@ -179,7 +179,7 @@ namespace Huggle
         void on_actionEdit_user_talk_triggered();
         void on_actionReconnect_IRC_triggered();
         void on_actionTag_2_triggered();
-        void on_actionRequest_deletion_triggered();
+        void on_actionRequest_speedy_deletion_triggered();
 
     private:
         //! Check if huggle is shutting down or not, in case it is message box is shown as well
