30e605aa46e4fe9cba49f522b5f09f7f35cab2fb
Aidan <jamie.fell@btinternet.com>
2013-10-12 13:24:25 +0100

    shit oops

--- huggle/huggleweb.cpp
+++ huggle/huggleweb.cpp
@@ -99,7 +99,7 @@ void HuggleWeb::DisplayDiff(WikiEdit *edit)
         size = "+" + QString::number(edit->Size);
     } else
     {
-        return;
+         size = QString::number(edit->Size);
     }
 
     if (edit->Summary == "")
