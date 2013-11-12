406aa3c0bcbc690b55974d9740967f72a1adc9e3
Petr Bena <benapetr@gmail.com>
2013-11-12 16:56:42 +0100

    fixed build

--- huggle/sessionform.cpp
+++ huggle/sessionform.cpp
@@ -8,7 +8,7 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
-#include "sessionform.h"
+#include "sessionform.hpp"
 #include "ui_sessionform.h"
 
 using namespace Huggle;
