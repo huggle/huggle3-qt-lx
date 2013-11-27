a156d9d0f4d1da6a6b15db8d897d26995c327f48
Petr Bena <benapetr@gmail.com>
2013-11-27 16:38:18 +0100

    improved scripts

--- debian.sh
+++ debian.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-echo "Enter version"
-read v
+if [ "$#" -lt 1 ];then
+    echo "Enter version"
+    read v
+else
+    v="$1"
+fi
 
 tar -zcf "huggle_"$v".orig.tar.gz" huggle
 mv huggle huggle-"$v"
