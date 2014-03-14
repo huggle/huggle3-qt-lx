16445e601c08c65d330dc8d21a66d7a94beb0712
Petr Bena <benapetr@gmail.com>
2014-03-14 14:37:45 +0100

    previous one was fucked up
    the encoding seems to differ now

--- huggle/configure.ps1
+++ huggle/configure.ps1
@@ -8,10 +8,13 @@ if (!(Test-Path "update.sh"))
     exit 1
 }
 
-echo "Checking if version.txt exists..."
-if (!(Test-Path "version.txt"))
+echo "Creating version.txt"
+if (Test-Path("..\.git\FETCH_HEAD"))
 {
-    echo "Creating version.txt (if you want to have the revision in huggle version, you need to run this with option --git)"
+    $first, $lines = cat "..\.git\FETCH_HEAD"
+    echo $first | %{$_ -replace " .*", ""} | Set-Content version.txt
+} else
+{
     echo "Development (non-git)" > version.txt
 }
 
@@ -31,13 +34,13 @@ if (!(Test-Path("huggle.tmp")) -and !(Test-Path("exception_handler.lib")))
 #!!!!!!!  #This line was replaced with power shell, DO NOT COMMIT THIS CHANGE
 #!!!!!!!  #LIBS +=  ..\huggle\exception_handler.lib ..\huggle\crash_generation_client.lib
 "@
-    cat "huggle.tmp" | %{$_ -replace ".*exception_handler\.lib.*", $replace} > huggle.pro
+    cat "huggle.tmp" | %{$_ -replace ".*exception_handler\.lib.*", $replace} | Set-Content huggle.pro
         $replace = @"
 // This line was replaced with power shell, DO NOT COMMIT THIS CHANGE
 #define DISABLE_BREAKPAD
 // End of modified code, nothing else changed
 "@
-    cat "definitions_prod.hpp" | %{$_ -replace ".*DISABLE_BREAKPAD.*", $replace} > definitions.hpp
+    cat "definitions_prod.hpp" | %{$_ -replace ".*DISABLE_BREAKPAD.*", $replace} | Set-Content definitions.hpp
 }
 
 echo "It's all done, you can build huggle now from withing Qt creator!"
