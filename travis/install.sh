58c09ffbe237544192c408a6755fc833361ff268
Petr Bena <benapetr@gmail.com>
2014-05-05 23:45:17 +0200

    let's try this

--- travis/install.sh
+++ travis/install.sh
@@ -9,7 +9,6 @@ if [ "$QTTYPE" = "5" ]; then
 fi
 
 sudo apt-get install g++-4.7
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
 sudo update-alternatives --config gcc
