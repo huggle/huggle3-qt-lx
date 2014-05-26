18a611c73f65921ff888c0cef8720c7e8bdff7b2
Petr Bena <benapetr@gmail.com>
2014-05-26 14:08:01 +0200

    updated travis

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -8,16 +8,18 @@ cd /home/travis/build/huggle/huggle3-qt-lx/huggle
 
 if [ "$QTTYPE" = "4" ]; then
 	./configure --qt4
+        cd huggle_prod
 	make
-	cd tests/test
+	cd ../tests/test
 	qmake
 	make
 fi
 
 if [ "$QTTYPE" = "5" ]; then
-	./configure --qt5 --qmake /usr/lib/x86_64-linux-gnu/qt5/bin/qmake
+	./configure --qt5
+        cd huggle_prod
 	make
-	cd tests/test
+	cd ../tests/test
 	/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 	make
 fi
