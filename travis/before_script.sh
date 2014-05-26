3f3635cfe65d9c0f3d4096c99b7277a22924a8b0
Petr Bena <benapetr@gmail.com>
2014-05-26 17:07:43 +0200

    in addition disable python on travis :P

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -8,6 +8,7 @@ cd /home/travis/build/huggle/huggle3-qt-lx/huggle
 
 if [ "$QTTYPE" = "4" ]; then
 	./configure --qt4
+        cp definitions_prod.hpp definitions.hpp
         cd huggle_prod
 	make
         cd ..
@@ -20,6 +21,7 @@ fi
 
 if [ "$QTTYPE" = "5" ]; then
 	./configure --qt5
+        cp definitions_prod.hpp definitions.hpp
         cd huggle_prod
 	make
         cd ..
