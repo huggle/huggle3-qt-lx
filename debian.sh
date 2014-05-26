199bdf1721b95e21ac6021e89b313294157a78a8
Petr Bena <benapetr@gmail.com>
2014-05-26 17:40:39 +0200

    no need for this

--- debian.sh
+++ debian.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-if [ "$#" -lt 1 ];then
-    echo "Enter version"
-    read v
-else
-    v="$1"
-fi
-
-tar -zcf "huggle_"$v".orig.tar.gz" huggle
-cp -r huggle huggle-"$v"
-cd huggle-"$v"
+cd huggle
 debuild -us -uc
