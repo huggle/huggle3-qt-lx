9de3bb85767c3c8d5efa08a323956c7fc495457b
Petr Bena <benapetr@gmail.com>
2014-05-31 18:38:51 +0200

    edit suffix should be that we have in the config

--- huggle/configuration.cpp
+++ huggle/configuration.cpp
@@ -517,6 +517,7 @@ bool Configuration::ParseProjectConfig(QString config)
               "Reverted edits by [[Special:Contributions/$1|$1]] ([[User talk:$1|talk]]) to last revision by $2");
     this->ProjectConfig_AgfRevert = ConfigurationParse("agf", config, "Reverted good faith edits by [[Special:Contributions/$2|$2]]"\
                                                        " [[User talk:$2|talk]]");
+    this->ProjectConfig_EditSuffixOfHuggle = ConfigurationParse("summary", config, "[[Project:Huggle|HG]]");
     this->ProjectConfig_WarnSummary3 = ConfigurationParse("warn-summary-3", config);
     this->ProjectConfig_WarnSummary4 = ConfigurationParse("warn-summary-4", config);
     this->ProjectConfig_RevertSummaries = HuggleParser::ConfigurationParse_QL("template-summ", config);
