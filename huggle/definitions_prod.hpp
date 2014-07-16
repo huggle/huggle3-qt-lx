eb5c91b4e963273af305eb2745053199d734ae60
Petr Bena <benapetr@gmail.com>
2014-07-16 17:16:44 +0200

    one more fix for clang compiler (mac only)

--- huggle/definitions_prod.hpp
+++ huggle/definitions_prod.hpp
@@ -35,6 +35,14 @@ typedef char byte_ht;
     #ifdef TARGET_OS_MAC
         #define HUGGLE_MACX true
         #define HUGGLE_NO_MT_GC
+        // this is a simple fix to problem with missing declaration in clang headers
+        namespace std
+        {
+            // this define is here so that we can somehow detect if we already have this thing defined or not
+            // since there is no other way to check if type was defined or not
+            #define HUGGLE_NULLPTR_T
+            typedef decltype(nullptr) nullptr_t;
+        }
     #endif
 #endif
 // this is a nasty hack that will disable multi threaded gc on MacOS as we had some report that
