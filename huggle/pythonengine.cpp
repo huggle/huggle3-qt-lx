f1442ebfd758f5ee7cf671e27b67192b1bbd7f4d
Petr Bena <benapetr@gmail.com>
2014-08-29 14:09:24 +0200

    removed extra configuration include o.O

--- huggle/pythonengine.cpp
+++ huggle/pythonengine.cpp
@@ -8,8 +8,6 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
-#include "configuration.hpp"
-
 #ifdef HUGGLE_PYTHON
 
 #include "pythonengine.hpp"
