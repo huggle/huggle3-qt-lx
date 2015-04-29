6134ec6a74513305d602c29c619924b1ef57359f
Petr Bena <benapetr@gmail.com>
2015-04-29 16:52:09 +0200

    fixed win64 openssl path to dll
    now it should finally work

--- windows64/pack.ps1
+++ windows64/pack.ps1
@@ -1,2 +1,2 @@
 ﻿# This just run the original script
-..\windows\release.ps1 -cmake_generator "Visual Studio 12 2013 Win64" -vcredist vcredist_x64.exe -python $false -qt5_path "C:\Qt\5.4\msvc2013_64\"
+..\windows\release.ps1 -cmake_generator "Visual Studio 12 2013 Win64" -vcredist vcredist_x64.exe -python $false -qt5_path "C:\Qt\5.4\msvc2013_64\" -openssl_path "C:\OpenSSL-Win64"
