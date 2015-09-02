dc6a68ed0ebe1299dc315c6607d5449f9f57c1bc
Petr Bena <benapetr@gmail.com>
2015-09-02 13:11:45 +0200

    version++

--- huggle/definitions_prod.hpp
+++ huggle/definitions_prod.hpp
@@ -16,12 +16,12 @@
 typedef char byte_ht;
 typedef long long revid_ht;
 
-#define HUGGLE_VERSION                  "3.1.15"
+#define HUGGLE_VERSION                  "3.1.16"
 #define HUGGLE_BYTE_VERSION_MAJOR       0x3
 #define HUGGLE_BYTE_VERSION_MINOR       0x1
-#define HUGGLE_BYTE_VERSION_RELEASE     0xF
+#define HUGGLE_BYTE_VERSION_RELEASE     0x10
 // format is 0xMAJOR(2)MINOR(2)RELEASE(2) so for 3.1.15 it's 0x03010F
-#define HUGGLE_BYTE_VERSION             0x03010F
+#define HUGGLE_BYTE_VERSION             0x030110
 
 // Version of mediawiki that we do support
 #define HUGGLE_SUPPORTED_MEDIAWIKI_VERSION "1.25"
