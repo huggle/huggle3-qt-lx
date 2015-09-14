82f7474a8f6abfc5179f043f4c4812de254ee1a0
Petr Bena <benapetr@gmail.com>
2015-09-14 17:07:29 +0200

    Resolved warning

--- huggle/syslog.hpp
+++ huggle/syslog.hpp
@@ -66,7 +66,7 @@ namespace Huggle
             static Syslog *HuggleLogs;
 
             Syslog();
-            ~Syslog();
+            virtual ~Syslog();
             //! Write text to terminal as well as ring log
             /*!
              * \param Message Message to log
