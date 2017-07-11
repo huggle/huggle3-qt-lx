52a9cacd4ad0d6c15d56ed7e1b309d8c99091bce
Petr Bena <benapetr@gmail.com>
2017-07-11 21:43:53 +0200

    made snapcraft work

--- linux/snap/generate_snap.sh
+++ linux/snap/generate_snap.sh
@@ -14,13 +14,20 @@ mkdir tmp || exit 1
 cd tmp || exit 1
 cp ../snapcraft.yaml .
 mkdir prime
+mkdir prime/bin
 mkdir prime/usr
 mkdir prime/usr/lib
+mkdir -p snap/gui
+mkdir -p prime/usr/share/huggle
 mkdir prime/lib
 mkdir -p prime/usr/share/huggle/extensions
 mkdir prime/usr/bin
-cp -r "$source" src
+cp -r "../$source" src
 rm -rf src/huggle_release
+cp src/Resources/huggle3_newlogo.png prime/usr/share/huggle/huggle3_newlogo.png
+cp src/build/huggle.desktop snap/gui/
+# Now this is a hack that fixes problem with lookup of Qt libraries, if you know about better one, improve it!!
+ln -s ../usr/lib/x86_64-linux-gnu/qt5/plugins/platforms prime/bin/platforms
 cd src || exit 1
 rm -rf version.txt definitions.hpp
 cp definitions_prod.hpp definitions.hpp
