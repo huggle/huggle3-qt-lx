0c8aa76176b18bb42c24e09b0b1a1a09aa4b494f
Petr Bena <benapetr@gmail.com>
2017-10-10 22:14:20 +0200

    update brew before running the CI tests

--- travis/before_install_osx.sh
+++ travis/before_install_osx.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+brew update
+
 exit 0
--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-rvm install ruby-2.3.3
-rvm --default use 2.3.3
+#rvm install ruby-2.3.3
+#rvm --default use 2.3.3
 
 if [ "$QTTYPE" = "4" ]; then
 	brew install qt4
