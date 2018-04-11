e6bcc3b820b62c871cc8a572897563bc52cef53b
Petr Bena <benapetr@gmail.com>
2018-04-11 21:05:38 +0200

    fix travis

--- travis/install.sh
+++ travis/install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ "$QTTYPE" = "4" ]; then
-	sudo apt-get install -y libqt4-webkit libqtwebkit-dev qt4-qmake qt4-dev-tools cmake
-fi
+#if [ "$QTTYPE" = "4" ]; then
+#	sudo apt-get install -y libqt4-webkit libqtwebkit-dev qt4-qmake qt4-dev-tools cmake
+#fi
 
-if [ "$QTTYPE" = "5" ]; then
-	sudo apt-get install -y ubuntu-sdk qtquick1-5-dev cmake
-fi
+#if [ "$QTTYPE" = "5" ]; then
+#fi
 
-sudo apt-get install g++-4.7
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+sudo apt-get install -y ubuntu-sdk qtquick1-5-dev cmake qtmultimedia5-dev qtbase5-dev qtdeclarative5-dev qt5-default qttools5-dev-tools qtscript5-dev
+#sudo apt-get install g++-4.7
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
 #sudo update-alternatives --config gcc
