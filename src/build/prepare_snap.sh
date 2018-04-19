e6ca1050e96705b5516fbcb6128a9acb418f59c9
Petr Bena <benapetr@gmail.com>
2018-04-19 22:22:25 +0200

    fixed snaps

--- src/build/prepare_snap.sh
+++ src/build/prepare_snap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cd ../src || exit 1
+echo "Preparing in `pwd`"
 git submodule init
 git submodule update
+cd huggle_core || exit 1
 sh update.sh
-cp definitions_prod.hpp definitions.hpp
-rm -rf "huggle_release"
+cp definitions_prod.hpp definitions.hpp || exit 1
