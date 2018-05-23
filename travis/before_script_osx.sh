61fb4b48198bf98c36a5be0a6ee1e92b76e998af
Petr Bena <benapetr@gmail.com>
2018-05-23 11:12:07 +0200

    fixed travis-ci for MacOS
    change in brew:
    
    This formula is keg-only, which means it was not symlinked into /usr/local,
    because Qt 5 has CMake issues when linked.

--- travis/before_script_osx.sh
+++ travis/before_script_osx.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+ls /usr/local/Cellar/qt/*
 if [ "$QTTYPE" = "5" ]; then
-    ./configure --tests --extension --web-engine --qtpath /usr/local/opt/qt/
+    ./configure --tests --extension --web-engine --qtpath /usr/local/Cellar/qt/*/clang_64
     cd release
     make || exit 1
 fi
