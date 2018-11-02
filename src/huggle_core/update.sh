f4065be46383a4e26b1a2fba67e73a2322baaa1a
Petr Bena <benapetr@gmail.com>
2018-11-02 11:10:37 +0100

    documentation

--- src/huggle_core/update.sh
+++ src/huggle_core/update.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# This file generates version.txt which is built into resulting binary and is used
+# to determine Huggle's actual version, including GIT revision number
+
+# In case this is not a git repo, git string is replaced simply with "production build"
+# these are remnants of past when production builds were running automatically from
+# WMF labs
+
+# This file is called from multiple places, most notably configure script
+
 if [ -d ../../.git ];then
   c=`git rev-list HEAD --count`
   hash=`git describe --always --tags`
