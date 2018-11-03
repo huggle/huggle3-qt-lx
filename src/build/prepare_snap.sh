1bd16b595477ce3a8bbe0b221f1e4669f2ff18f9
Luke Faraone <luke@faraone.cc>
2018-11-03 05:21:04 +0000

    snap: Fix FTBFS
    Closes #307
    
    Handle path differences that changed in between versions of ``snapcraft``.
    Drop explicit submodule handling in favour of ``source-type: git``.

--- src/build/prepare_snap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ../src || exit 1
-echo "Preparing in `pwd`"
-git submodule init
-git submodule update
-cd huggle_core || exit 1
-sh update.sh
-cp definitions_prod.hpp definitions.hpp || exit 1
