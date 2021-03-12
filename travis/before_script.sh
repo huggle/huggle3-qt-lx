37529cd8fad18ed2a09e7264ac414f318fd1918e
phuzion <398094+phuzion@users.noreply.github.com>
2021-03-12 07:39:40 +0000

    Improve GitHub Actions workflow, matrices are now used to test on multiple versions of Qt and multiple OSes where available (Windows and Ubuntu).

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -4,7 +4,7 @@ g++ --version
 cmake --version
 
 if [ "$QTTYPE" = "5" ]; then
-    ./configure --tests --extension
+    ./configure --tests --extension --web-engine
     cd release
     make || exit 1
 fi
