0fbce69f816790ab614f616109e682b5c1fce7ef
Jatin Sharma <js@bb.com>
2021-04-04 02:41:22 +0530

    add delay of 2sec before closing window
    singleshot timer is used with a lambda function.

--- src/huggle_ui/reportuser.cpp
+++ src/huggle_ui/reportuser.cpp
@@ -324,7 +324,10 @@ void ReportUser::OnReportUserTimer()
                 //this->close();
                 delete this;
             }
-            this->close();
+            // Close window after 2 seconds
+            QTimer::singleShot(2000, [this]()->void{
+                this->close();
+            });
         }
         return;
     }
