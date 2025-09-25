857ad512a2340782668c6f90aba55bac61f59051
Petr Bena <benapetr@gmail.com>
2025-09-25 13:30:49 +0200

    fixes

--- travis/before_script_osx.sh
+++ travis/before_script_osx.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-./configure --qt6 --tests --extension --web-engine --qtpath /usr/local/opt/qt/
+# this qtpath is for Qt5 only
+#./configure --qt6 --tests --extension --web-engine --qtpath /usr/local/opt/qt/
+./configure --qt6 --tests --extension --web-engine --qtpath /Users/runner/work/huggle3-qt-lx/Qt/6.5.3/macos/
 cd release
 make || exit 1
