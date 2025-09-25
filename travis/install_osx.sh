9eb53b08323ad100b998041d0bf4164537ea1528
Petr Bena <benapetr@gmail.com>
2025-09-25 12:29:05 +0200

    fixing macos

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-#rvm install ruby-2.3.3
-#rvm --default use 2.3.3
-
-if [ "$QTTYPE" = "4" ]; then
-	brew install qt4
-fi
-
-if [ "$QTTYPE" = "5" ]; then
-	brew install qt
-fi
+exit 0
 
