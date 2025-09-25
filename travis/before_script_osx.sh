9d1c2273b7e892a1b0b1ba0876e09bb9da2652a7
Petr Bena <benapetr@gmail.com>
2025-09-25 13:06:02 +0200

    drop Qt5 from macos builds

--- travis/before_script_osx.sh
+++ travis/before_script_osx.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-if [ "$QTTYPE" = "5" ]; then
-    ./configure --tests --extension --web-engine --qtpath /usr/local/opt/qt/
-    cd release
-    make || exit 1
-fi
+./configure --qt6 --tests --extension --web-engine --qtpath /usr/local/opt/qt/
+cd release
+make || exit 1
